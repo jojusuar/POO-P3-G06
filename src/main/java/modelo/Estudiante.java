7281aa9a9cf4ac48c6ce88675c42e398d37c7a15
angbeand <angbeand@espol.edu.ec>
2023-07-10 23:07:07 -0500

    pruebita en clase estudiante

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -10,5 +10,18 @@ package modelo;
  * @author jojusuar
  */
 public class Estudiante {
+    private String nombre;
+    private String correo;
+    private String matricula;
+  public Estudiante(String n, String c, String m){
+    nombre = n;
+    correo = c;
+    matricula = m;
+  }
+  public void usarComodin(Tipo comodin t){
     
+  }
+    
+
 }
+
