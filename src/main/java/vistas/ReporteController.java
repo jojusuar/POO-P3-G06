3b241eeb70ead1efca4ab6689843e3f91531e83a
MateoVE <juamvill@espol.edu.ec>
2023-08-23 16:32:02 -0500

    Se implementaron cosas de reporte

--- src/main/java/vistas/ReporteController.java
+++ src/main/java/vistas/ReporteController.java
@@ -17,6 +17,9 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
+import modelo.*;
+import java.util.ArrayList;
+import java.io.*;
 
 /**
  * FXML Controller class
@@ -24,21 +27,89 @@ import javafx.scene.layout.VBox;
  * @author DELL
  */
 public class ReporteController implements Initializable {
+    
+    @FXML
+    private VBox vb;
+    @FXML
     private Button generarReporte;
+    @FXML
+    private Button regresar;
+    @FXML
     private TextField entradaPeriodo;
+    @FXML
     private TextField entradaCodigoMateria;
+    @FXML
     private TextField entradaParalelo;
-
+    
+    
+    String entrada = "";
+    String codigoMateria = "";
+    String paralelo = "";
+    
     @FXML
     private void generarReporte()throws Exception{
-        App.setRoot(null);
+        entrada = entradaPeriodo.getText();
+        codigoMateria = entradaCodigoMateria.getText();
+        paralelo = entradaParalelo.getText();
+        ArrayList<Juego> listaJuegos = new ArrayList<>();
+        ArrayList<Juego> juegosReporte;
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("src/main/resources/memory/juego.ser"))){
+            Juego juego;
+            while(true){
+                try{
+                    juego = (Juego)in.readObject();
+                    System.out.println(juego+"");
+                    listaJuegos.add(juego);
+                }catch(EOFException e) {
+                    System.out.println("ya no hay mas objetos");
+                    break;
+                }
+            }
+           
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no encontrado");
+        } catch (IOException ex) {
+            System.out.println("IOException");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Clase no encontrada");
+        }
+        System.out.println(listaJuegos.size());
+        try{
+            juegosReporte = UtilitariaReporte.generarReporte(entrada, codigoMateria, paralelo, listaJuegos);
+        }catch(Exception e){
+            System.out.println("Datos incompletos o incorrectos");
+            juegosReporte = new ArrayList<>();
+        }
+        DatosReporte datos = new DatosReporte();
+        datos.setListaJuegos(juegosReporte);
+        PresentacionReporteController presentacion = new PresentacionReporteController();
+        presentacion.recibirDatos(datos);
+        App.setRoot("PresentacionReporte");
+    }
+    
+    
+    
+    @FXML
+    private void regresarMenuPrincipal()throws Exception{
+        App.setRoot("primary");
     }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+    }
     
+}
+class DatosReporte{
+    private ArrayList<Juego> listaJuegos;
+    //getter
+    public ArrayList<Juego> getListaJuegos(){
+        return listaJuegos;
+    }
+    //Setters
+    public void setListaJuegos(ArrayList<Juego> listaJuegos){
+        this.listaJuegos = listaJuegos;
+    }
 }
