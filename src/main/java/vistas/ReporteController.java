2b53d16754dac80adcdcd7b111e24479175090a2
MateoVE <juamvill@espol.edu.ec>
2023-08-27 22:08:52 -0500

    Comentado

--- src/main/java/vistas/ReporteController.java
+++ src/main/java/vistas/ReporteController.java
@@ -22,7 +22,7 @@ import java.io.*;
  * @author DELL
  */
 public class ReporteController implements Initializable {
-    
+    //Se definen los elementos graficos on sus variables respectivas
     @FXML
     private VBox vb;
     @FXML
@@ -36,35 +36,45 @@ public class ReporteController implements Initializable {
     @FXML
     private TextField entradaParalelo;
     
-    
+    //Se definen varibales inicializadas vacias, para despues darles el valor de los textfields
     String entrada = "";
     String codigoMateria = "";
     String paralelo = "";
-    
+    //Se define la funcion generar reporte que se ejecuta cuando se presiona el boton d egenerar reporte en el menu Repoorte
     @FXML
     private void generarReporte()throws Exception{
+        //Solicita la entrada de los textfields
         entrada = entradaPeriodo.getText();
         codigoMateria = entradaCodigoMateria.getText();
         paralelo = entradaParalelo.getText();
+        //Se crean Arraylist vacios, en los cuales se almacenaran los listados de juegos y los listados de juegos que cumplen con los parametros
         ArrayList<Juego> listaJuegos;
         ArrayList<Juego> juegosReporte;
+        //Se crea un nuevo objeto de tipo DatosReporte, que es una clase que nos ayudara a enviar la informacion de esta clase a la clase PresentacionResporteController
         DatosReporte datos = new DatosReporte();
+        //Se abre el stream de datos para obtener el ArrayList de juegos
         try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("src/main/resources/memory/juegoshistorial.ser"))){
+            //Se usa otro try por si el objeto esta vacio
             try{
                 listaJuegos = (ArrayList<Juego>)in.readObject();
                 System.out.println("Juegos registrados: "+listaJuegos.size());
+                //Se usa otro try por si la funcuon generarReporte presenta un error
                 try{
                     System.out.println("Reportes que coinciden:");
+                    //Esta funcion presenta error cuando la entrada de termino no esta separada por "-", y tiene un size diferente a 2
                     juegosReporte = UtilitariaReporte.generarReporte(entrada, codigoMateria, paralelo, listaJuegos);
                 }catch(Exception e){
+                    //Manejo de error de generarReporte
                     System.out.println("Datos incompletos o incorrectos");
                     juegosReporte = new ArrayList<>();
                 }
+                //Se asigna el valor de la lista de juegos en el objeto DatosReporte como el ArrayList de juegos coincidentes
                 datos.setListaJuegos(juegosReporte);
-                
+            //Se maneja el error de datos incorrectos
             }catch(EOFException e) {
                 System.out.println("no es de este tipo");
             }
+        //Se maneja todos los posibles errores generados por el primer try
         }catch(FileNotFoundException ex){
             System.out.println("Archivo no encontrado");
         }catch(IOException ex){
@@ -73,13 +83,15 @@ public class ReporteController implements Initializable {
         }catch(ClassNotFoundException ex){
             System.out.println("Clase no encontrada");
         }
+        //Se crea un objeto PresentacionReporteController, y se asigna datos como su atributo de clase Datos reporte
         PresentacionReporteController presentacion = new PresentacionReporteController();
         presentacion.recibirDatos(datos);
+        //Cambia de pantalla a la generada por PresentacionReporteController
         App.setRoot("PresentacionReporte");
     }
     
     
-    
+    //Metodo que permite volver en caso de presionar el boton regresar
     @FXML
     private void regresarMenuPrincipal()throws Exception{
         App.setRoot("primary");
@@ -95,6 +107,8 @@ public class ReporteController implements Initializable {
     }
     
 }
+
+//Definicion de la clase DatosReporte que nos ayudara a enviar los datos a la clase Presentacion Reporte Controller
 class DatosReporte{
     private ArrayList<Juego> listaJuegos;
     
